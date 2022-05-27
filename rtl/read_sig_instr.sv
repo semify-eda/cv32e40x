60060d23c676c8e9903b5b5a06ee38cb905332b3
Mario Freisinger <m.freisinger@student.tugraz.at>
2022-05-28 00:15:52 +0200

    ext if module connected to core

--- rtl/read_sig_instr.sv
+++ rtl/read_sig_instr.sv
@@ -1,13 +1,15 @@
 module read_sig_instr (input clk_i,
                        input rst_ni,
-                       if_xif.coproc_issue xif_issue);
+                       if_xif.coproc_issue xif_issue );
 
+
+  //assign xif_issue.issue_ready = 1'b1;
 
   
   always_ff @(posedge clk_i, negedge rst_ni) begin
     if (!rst_ni) begin
     end else begin
-      if (xif_issue.issue_req)
+      if (xif_issue.issue_req != 0)
         $display("xif_issue = %h", xif_issue.issue_req);
       
     end
