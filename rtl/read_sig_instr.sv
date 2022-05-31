9a239414aa31968bdb0fd2cac7cde944f2ca3053
Mario Freisinger <m.freisinger@student.tugraz.at>
2022-05-31 16:40:47 +0200

    valid and accept singaling works

--- rtl/read_sig_instr.sv
+++ rtl/read_sig_instr.sv
@@ -10,7 +10,6 @@ module read_sig_instr (input clk_i,
 
   logic                      issue_ready_n, issue_ready_p;
   logic                      issue_accept_n, issue_accept_p;
-  logic [31:0]               instr_n, instr_p;
                       
   
 
@@ -18,9 +17,8 @@ module read_sig_instr (input clk_i,
   assign xif_compressed.compressed_resp.accept = 1'b0;
   assign xif_compressed.compressed_resp.instr = 1'b0;
 
-  assign xif_issue.issue_ready = 1'b1;
-// issue_ready_p;
-  assign xif_issue.issue_resp.accept = 1'b1; //issue_accept_p; //ready 1 and accept 0 to reject all offloaded instrucitons for now
+  assign xif_issue.issue_ready = issue_ready_p;
+  assign xif_issue.issue_resp.accept = issue_accept_p; //ready 1 and accept 0 to reject all offloaded instrucitons for now
   assign xif_issue.issue_resp.writeback = 1'b0;
   assign xif_issue.issue_resp.dualwrite = 1'b0;
   assign xif_issue.issue_resp.dualread = 1'b0;
@@ -39,7 +37,7 @@ module read_sig_instr (input clk_i,
   
   assign xif_mem.mem_valid = 1'b0;
 
-  assign xif_result.result_valid = 1'b1;
+  assign xif_result.result_valid = 1'b0;
 
   assign xif_result.result.id = 0;
   assign xif_result.result.data = 0;
@@ -53,15 +51,12 @@ module read_sig_instr (input clk_i,
   always_comb begin
     issue_ready_n = 1'b0;
     issue_accept_n = 1'b0;
-    instr_n = xif_issue.issue_req.instr;
     
     if (xif_issue.issue_valid) begin
       issue_ready_n = 1'b1;
-      instr_n = instr_p; //store instruction as long as valid is high
-                         // cuz it is undefeined as soon as valid is high
-      if (instr_p[7:0] == 8'h6f) begin
+     
+      if (xif_issue.issue_req.instr[6:0] == 7'h3b) begin
         issue_accept_n = 1'b1;
-        issue_ready_n = 1'b1;
       end
     end        
       
@@ -72,11 +67,9 @@ module read_sig_instr (input clk_i,
     if (!rst_ni) begin
       issue_ready_p <= 1'b0;
       issue_accept_p <= 1'b0;
-      instr_p <= 32'h0;
     end else begin
       issue_ready_p <= issue_ready_n;
-      issue_accept_p <= issue_accept_n;
-      instr_p <= instr_n;      
+      issue_accept_p <= issue_accept_n;    
     end
   end
   
