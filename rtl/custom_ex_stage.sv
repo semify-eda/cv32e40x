79643974d176f227fd3e72d76ca7e9383e70ac0b
Mario Freisinger <m.freisinger@student.tugraz.at>
2022-06-08 23:05:02 +0200

    using generate to instantiate count bits modules

--- rtl/custom_ex_stage.sv
+++ rtl/custom_ex_stage.sv
@@ -41,15 +41,30 @@ module custom_ex_stage import custom_instr_pkg::*;
   end // always_comb
 
   logic [31:0] top_4_bit, top_3_bit, top_2_bit;
-  logic [31:0] top_bits_set [4];
+  logic [31:0] top_bits_set [3:0];
   
-  logic [8:0] right_shift;
+  logic [6:0] right_shift;
+  logic [31:0] result_cntb [3:0];
+  
   
 
   assign right_shift = 31 - rs1;
+
+   genvar      geni;
+   generate
+     for (geni = 0; geni < 3; geni = geni + 1) begin
+       count_bits #(.top_many_bits (geni)) cb
+       (
+        .rs0_i (rs0),
+        .right_shift_i (right_shift),
+        .top_bits_set_i (top_bits_set[geni]),
+        .result_o (result_cntb[geni])
+        );
+     end
+   endgenerate
+ 
   
-  integer     i;
-  
+  integer     i;  
   //exec  logic for cntb instruction
   always_comb begin
     rd_DN = rd_DP;
@@ -62,17 +77,25 @@ module custom_ex_stage import custom_instr_pkg::*;
     if (issue_ready_SP == 0) begin
       if (rs0[rs1] == 1'b1) begin
         // count consecutive 1 bits
-        if ((rs0 & ((top_bits_set[0])>>right_shift)) == top_bits_set[0]>>right_shift)
+        if ((rs0 & result_cntb[0]) == top_bits_set[0]>>right_shift)
           rd_DN = 32'd4;
-        else
-        if ((rs0 & ((top_bits_set[1])>>right_shift)) == top_bits_set[1]>>right_shift)
+        else if ((rs0 & result_cntb[1]) == top_bits_set[1]>>right_shift)
           rd_DN = 32'd3;
-        else
-        if ((rs0 & ((top_bits_set[2])>>right_shift)) == top_bits_set[2]>>right_shift)
+        else if ((rs0 & result_cntb[2]) == top_bits_set[2]>>right_shift)
           rd_DN = 32'd2;
         else
           rd_DN = 32'd1;    
       end else if (rs0[rs1] == 1'b0) begin // if (rs0[rs1] == 1'b1)
+        // count consecutive 0 bits
+        if ((rs0 & result_cntb[0]) == 32'd0)
+          rd_DN = 32'd4;
+        else if ((rs0 & result_cntb[1]) == 32'd0)
+          rd_DN = 32'd3;
+        else if ((rs0 & result_cntb[2]) == 32'd0)
+          rd_DN = 32'd2;
+        else
+          rd_DN = 32'd1;   
+      end
         
     end
 
