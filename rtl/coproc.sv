14fbc27f03537535edf2d12dcb347d1596ea58b1
Mario Freisinger <m.freisinger@student.tugraz.at>
2022-06-14 20:53:51 +0200

    started with wbits instruction

--- rtl/coproc.sv
+++ rtl/coproc.sv
@@ -10,10 +10,16 @@ module coproc import custom_instr_pkg::*;
    if_xif.coproc_result xif_result 
    );
 
-  enum   logic [1:0] {EXECUTE, WRITEBACK} state_SN, state_SP;
+  enum   logic [1:0] {INIT, EXECUTE, MEMORY, WRITEBACK} state_SN, state_SP;
   
   logic [31:0] rd_i;
   logic [4:0]  rd_addr_i;
+  logic        mem_needed_i;
+
+  logic        ld_op_SN, ld_op_SP;
+  logic        cntb_start_SP, cntb_start_SN, wbits_start_SP, wbits_start_SN;
+  
+  
 
   assign xif_compressed.compressed_ready = 1'b0;
   assign xif_compressed.compressed_resp.accept = 1'b0;
@@ -24,20 +30,9 @@ module coproc import custom_instr_pkg::*;
   //assign xif_issue.issue_resp.writeback = 1'b0;
   assign xif_issue.issue_resp.dualwrite = 1'b0;
   assign xif_issue.issue_resp.dualread = 1'b0;
-  assign xif_issue.issue_resp.loadstore = 1'b0;
+  assign xif_issue.issue_resp.loadstore = ld_op_SP;
   assign xif_issue.issue_resp.ecswrite = 1'b0;
   assign xif_issue.issue_resp.exc = 1'b0;
-
-  assign xif_mem.mem_req.id = 0;
-  assign xif_mem.mem_req.addr = 0;
-  assign xif_mem.mem_req.mode = 0;
-  assign xif_mem.mem_req.we = 0;
-  assign xif_mem.mem_req.be = 0;
-  assign xif_mem.mem_req.wdata = 0;
-  assign xif_mem.mem_req.last = 0;
-  assign xif_mem.mem_req.spec = 0;
-  
-  assign xif_mem.mem_valid = 1'b0;
 
   assign xif_result.result.id = 0;
   assign xif_result.result.data = rd_i;
@@ -49,13 +44,24 @@ module coproc import custom_instr_pkg::*;
   assign xif_result.result.exccode = 0;
   
 
-  custom_ex_stage custom_ex_stage_i
+  // hardware for cntb instruction
+  cntb cntb_i
     (
      .clk_i (clk_i),
      .rst_ni (rst_ni),
      .rd_o (rd_i),
      .xif_issue (xif_issue),
      .rd_addr_o (rd_addr_i)
+     );
+
+  // hardware for stroing bits unaligned after decoding
+  wbits wbits_i
+    ( .clk_i (clk_i),
+      .rst_ni (rst_ni),
+      .xif_issue (xif_issue),
+      .xif_mem (xif_mem),
+      .xif_mem_result (xif_mem_result),
+      .mem_needed_o (mem_needed_i)
      );
   
   
@@ -65,9 +71,40 @@ module coproc import custom_instr_pkg::*;
     xif_result.result_valid = 1'b0;
 
     unique case (state_SP)
+      INIT: begin
+        if (xif_issue.issue_valid) begin
+          case (xif_issue.issue_req.instr[6:0])
+        
+            OPCODE_CNTB: begin
+            
+              ld_op_SN = 1'b0;
+              
+            end
+
+            OPCODE_WBITS: begin
+              ld_op_SN = 1'b1;
+            end
+          endcase // case (xif_issue.issue_req.instr[6:0])
+
+          state_SN = EXECUTE;
+        end // if (xif_issue.issue_valid)   
+      end // case: INIT
+      
+
+
       
       EXECUTE: begin
-        if (xif_issue.issue_ready) //exec stage is done
+        if (xif_issue.issue_ready) begin//exec stage is done
+           
+          if (mem_needed_i)
+            state_SN = MEMORY;
+          else
+            state_SN = WRITEBACK;
+        end
+      end
+
+      MEMORY: begin
+        if (!mem_needed_i)
           state_SN = WRITEBACK;
       end
 
@@ -83,9 +120,15 @@ module coproc import custom_instr_pkg::*;
   //Datapath
   always_ff @(posedge clk_i, negedge rst_ni) begin
     if (!rst_ni) begin
-      state_SP <= EXECUTE;
+      state_SP <= INIT;
+      cntb_start_SP <= 0;
+      wbits_start_SN <= 0;
+      ld_op_SP <= 0;
     end else begin
-      state_SP <= state_SN;     
+      state_SP <= state_SN;
+      cntb_start_SP <= cntb_start_SN;
+      wbits_start_SP <= wbits_start_SN;
+      ld_op_SP <= ld_op_SN;
     end
   end
   
--- rtl/include/custom_instr_pkg.sv
+++ rtl/include/custom_instr_pkg.sv
@@ -1,7 +1,8 @@
 package custom_instr_pkg;
 
   typedef enum logic [6:0] {
-                            OPCODE_CNTB = 7'h6b
+                            OPCODE_CNTB = 7'h6b,
+                            OPCODE_WBITS = 7'h7
                             } cust_opcodes_e;
 
 endpackage
